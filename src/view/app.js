c4a1e2ce2ceeff58a6b4d613b4a0750c1503ee1f
MaiZiYan <976007239@qq.com>
2019-03-15 17:40:38 +0800

    lib.less

--- src/view/app.js
+++ src/view/app.js
@@ -9,9 +9,21 @@ import Topics from './topics';
 export default class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Route exact path="/" component={Topics}></Route>
-            </BrowserRouter>
+            <div className="container">
+                <div className="header">
+                    <span className="title">React + React Router + Redux</span> 
+                </div>
+                <div className="content">
+                    <div className="nav">
+                        <a href="javascript:void(0)">cNode</a>
+                    </div>
+                    <div className="module">
+                        <BrowserRouter>
+                            <Route exact path="/" component={Topics}></Route>
+                        </BrowserRouter>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
